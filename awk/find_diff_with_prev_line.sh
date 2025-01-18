a096aa7cfd7f230be2e8d654ed377a0295e30d50
vishwanath <viz.dpatil@gmail.com>
2025-01-18 10:25:31 -0800

    Shorten long line for better readability

--- awk/find_diff_with_prev_line.sh
+++ awk/find_diff_with_prev_line.sh
@@ -1,1 +1,2 @@
-cat test_input/find_diff_with_prev_line/input.txt | awk 'BEGIN{first_line=1;prev=0} {if (!first_line) print ($1-prev);prev=$1;first_line=0}'
+input_file=test_input/find_diff_with_prev_line/input.txt
+cat $input_file | awk 'BEGIN{first_line=1;prev=0} {if (!first_line) print ($1-prev);prev=$1;first_line=0}'
